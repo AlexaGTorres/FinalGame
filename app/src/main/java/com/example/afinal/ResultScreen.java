a21b080d6d35000495a763676244a2ef9f877476
AlexaGTorres <alexa.torres19122694@gmail.com>
2022-05-31 08:50:58 -0700

    Added "Play Again" button function, when clicked it goes back to the first screen.

--- app/src/main/java/com/example/afinal/ResultScreen.java
+++ app/src/main/java/com/example/afinal/ResultScreen.java
@@ -2,13 +2,32 @@ package com.example.afinal;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class ResultScreen extends AppCompatActivity {
+public class ResultScreen extends AppCompatActivity
+{
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result_screen);
+        playAgainButton();
     }
-}
+
+    public void playAgainButton()
+    {
+        Button playAgainButton = (Button) findViewById(R.id.playAgainButton);
+        playAgainButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                startActivity(new Intent(ResultScreen.this, MainActivity.class));
+            }
+        });
+    }//End play again button handler
+}//End ResultScreen Class
