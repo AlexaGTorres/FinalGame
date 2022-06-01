44cc0192c8b4bdd602c31d709565297426980fc3
AlexaGTorres <alexa.torres19122694@gmail.com>
2022-06-01 08:43:36 -0700

    Made it possible to choose different colored cats.

--- app/src/main/java/com/example/afinal/MainActivity.java
+++ app/src/main/java/com/example/afinal/MainActivity.java
@@ -6,9 +6,14 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity
 {
+    ImageView img;
+    Button b;
+    int flag = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -16,6 +21,32 @@ public class MainActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         configurePlayButton();
+
+        img = (ImageView) findViewById(R.id.charImage);
+        b = (Button) findViewById(R.id.changeButton);
+
+        b.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                if (flag == 0)
+                {
+                    img.setImageResource(R.drawable.cat);//temp image
+                    flag = 1;
+                }
+                else if (flag == 1)
+                {
+                    img.setImageResource(R.drawable.cat1);
+                    flag = 2;
+                }
+                else if (flag == 2)
+                {
+                    img.setImageResource(R.drawable.cat2);
+                    flag = 0;
+                }
+            }
+        });//Check box
     }
 
     private void configurePlayButton()
